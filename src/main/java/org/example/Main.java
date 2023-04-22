60849bcf32f6a112114d3875a0ac49031ddcdf37
Iulia Cornea <iulia.cornea5@gmail.com>
2023-04-22 12:25:01 +0300

    add database configuration and connect to db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dbLocation = "localhost:3306";
+        String dbName = "jdbc_db";
+        String dbUser = "root";
+        String dbPassword = "root"; // parola voastră pentru baza de date
+
+        // MysqlDataSource <- vine din mysql connector și o folosim ca să configurăm conexiunea la baza de date
+        MysqlDataSource dataSource = new MysqlDataSource();
+        // Formatul pentru url-ul de conectare la baza de date
+        // jdbc:mysql://<<locația server-ului de baze de data>>/<<numele bazei de date>>
+        // jdbc:mysql://localhost:3306/jdbc_db
+        dataSource.setUrl("jdbc:mysql://" + dbLocation + "/" + dbName);
+        dataSource.setUser(dbUser);
+        dataSource.setPassword(dbPassword);
+
+        try {
+            LOGGER.log(Level.INFO, "Trying to connect to DB");
+            Connection connection = dataSource.getConnection();
+            LOGGER.log(Level.INFO, "Connection successful");
+        } catch (SQLException sqlException) {
+            LOGGER.log(Level.SEVERE,
+                    "Error when connecting to database " + sqlException.getMessage());
+        }
     }
 }
